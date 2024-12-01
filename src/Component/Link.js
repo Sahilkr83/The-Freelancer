d7062bf9bfebc5b38e40d0fac440d32783ba8bd7
Sahilkr83 <sahilkrsingh83@gmail.com>
2024-12-01 23:09:05 +0530

    some message

--- src/Component/Link.js
+++ src/Component/Link.js
@@ -1,9 +1,9 @@
 export const Link = [
     {
-        src:"https://www.youtube.com/embed/pTcSJgknBp0?si=p-pk49_-4qNqasSy"
+        src:"https://www.youtube.com/embed/ismXpDKMr9Q?si=HM4BwJ06vVs-DyuB"
     },
     {
-        src:"https://www.youtube.com/embed/UOz_qAFC1do?si=Jt3j9OWUD2vY_cEO" 
+        src:"https://www.youtube.com/embed/LvxTrRxB6Ck?" 
     }
 ]
 
