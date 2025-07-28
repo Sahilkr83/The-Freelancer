d71b19aec9f9395eacfab288d91d4e4d881c1b85
Sahil Singh <sahilkrsingh83@gmail.com>
2025-07-28 12:21:33 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -83,7 +83,7 @@ function App() {
     <div className="relative w-[100vw] wrapper h-[100vh] overflow-x-hidden bg-slate-950">
       <div className="absolute bottom-0 left-0 right-0 -top-36 z-10 bg-[radial-gradient(circle_500px_at_50%_200px,#3e3e3e,transparent)]"></div>
      
-      // {loading ? <Preloader fadeOut={fadeOut} /> : }
+      {/* {loading ? <Preloader fadeOut={fadeOut} /> : } */}
         <NewWeb />
     </div>
   );
