a77fce1adddb3722651d728ce2fa4ce97f47dac3
Sahilkr83 <sahilkrsingh83@gmail.com>
2025-08-04 11:17:40 +0530

    Image fix

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -7,11 +7,18 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
+        hostname: 'd33wubrfki0l68.cloudfront.net',
+        pathname: '/**', // allow all paths
+      },
+      {
+        protocol: 'https',
         hostname: 'app.netlify.com',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'api.dicebear.com',
+        pathname: '/**',
       },
     ],
   },
